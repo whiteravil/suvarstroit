cb4ca78ce1faecbf6a3146bedd01e6cf2d466178
Ravil <whiteravil@yandex.ru>
2021-02-15 18:46:31 +0300

    new

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,19 @@
 $(function() {
 
+	const touchPrevent = e => {
+		e.preventDefault();
+	}
+
+	function bodyNoScroll() {
+		$('body').addClass('no-scroll');
+  	document.addEventListener('touchmove', touchPrevent, { passive: true });
+	}
+
+	function bodyHasScroll() {
+		$('body').removeClass('no-scroll');
+  	document.removeEventListener('touchmove', touchPrevent, { passive: true });
+	}
+
 	let prevArrow = '<div class="slider-nav-arrow slider-prev"><span class="icon-angle-left"></span></div>',
 			nextArrow = '<div class="slider-nav-arrow slider-next"><span class="icon-angle-right"></span><svg class="progress-ring" width="39" height="39"><circle class="progress-ring-circle" stroke-width="1" stroke="#171723" cx="19.5" cy="19.5" r="19" fill="transparent" /></svg></div>';
 
@@ -127,7 +141,7 @@ $(function() {
 	let unhoverBannerTimeout;
 
 	$('.main-window-projects-nav a').hover(function() {
-		let thsId = $(this).attr('href'),
+		let thsId = $(this).data('tab-id'),
 				notCurrTab = $(`.main-window-projects-item:not(${thsId})`);
 		notCurrTab.removeClass('active');
 		$(thsId).addClass('active');
@@ -147,6 +161,118 @@ $(function() {
 		infinite: false,
 		prevArrow: $('.news-slider-arrow-prev'),
 		nextArrow: $('.news-slider-arrow-next')
-	})
+	});
+
+	function openPopup(id) {
+  	let notCurrentPopups = $(`.popup-wrapper:not(${id})`);
+  	notCurrentPopups.removeClass('opened');
+  	setTimeout(() => {
+  		notCurrentPopups.hide()
+  	}, 400);
+  	$(id).show();
+  	setTimeout(() => {
+  		$(id).addClass('opened')
+  	}, 50);
+  	bodyNoScroll();
+  }
+
+  function closePopup(e) {
+  	e !== undefined ? e.preventDefault() : '';
+  	$('.popup-wrapper').removeClass('opened');
+  	setTimeout(() => {
+  		$('.popup-wrapper').hide();
+  		$('.video-popup-block').html('');
+  	}, 400);
+  	bodyHasScroll();
+  }
+
+  $('.open-popup').on('click', function(e) {
+  	e.preventDefault();
+  	let id = $(this).attr('href');
+  	openPopup(id);
+  });
+
+  $('.popup-close').on('click', closePopup);
+
+  $('.popup-wrapper.opened').on('click', function(e) {
+  	let clickTarget = $(e.target);
+  	if ( clickTarget.hasClass('popup-flex') ) {
+  		closePopup();
+  	}
+  });
+
+  $('.video-block').each(function() {
+  	let ths = $(this),
+  			src = ths.data('video-src');
+  	ths.find('.video-play').on('click', function() {
+  		$('.video-popup-block').html(`<iframe width="560" height="315" src="${src}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+  		openPopup('#video-popup');
+  	});
+  });
+
+  let scrollTextArr = [];
+
+  $('.s-text-slider').each(function() {
+  	scrollTextArr.push(0);
+  });
+
+  $('.text-slider').slick({
+  	infinite: true,
+  	arrows: false,
+  	dots: false,
+  	draggable: false,
+  	autoplay: true,
+  	autoplaySpeed: 0,
+  	cssEase: 'linear',
+  	variableWidth: true,
+  	speed: 6000
+  });
+
+  let canTextAnimate = true;
+
+  function scrollText() {
+  	if ( canTextAnimate ) {
+  		canTextAnimate = false;
+  		$('.text-slider-wrapper').each(function() {
+  			let ths = $(this);
+	  			ths.css({
+		  		'transform': `translate3d(${-ths.find('.text-slider-item').eq(0).outerWidth()}px, 0, 0)`
+		  	})
+		  	setTimeout(() => {
+		  		ths.css({
+		  			'transition': 'all 0s ease',
+		  			'transform': `translate3d(0, 0, 0)`
+		  		})
+		  	}, 2500)
+		  	setTimeout(() => {
+		  		ths.attr('style', '')
+		  		canTextAnimate = true
+		  	}, 2550)
+  		})
+  	}
+  }
+
+  function projectHelpInfoPosition() {
+  	$('.project-price-info-dropdown').each(function() {
+  		let ths = $(this);
+  		if ( ths.offset().left + ths.outerWidth() > $(window).width() ) {
+  			ths.addClass('right-position')
+  		}
+  		else {
+  			ths.removeClass('right-position')
+  		}
+  	});
+  }
+
+  $(window)
+  .on('scroll', function() {
+  	scrollText()
+  })
+  .on('load', function() {
+  	projectHelpInfoPosition()
+  })
+  .on('resize', function() {
+  	projectHelpInfoPosition()
+  });
 
 });
